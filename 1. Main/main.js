31b727377242ae008f30589b16ab16674e3f5f5e
Brian Lai <kenlock.lai@gmail.com>
2022-08-22 18:07:15 +0100

    Subject:secondary nav bar build
    Change TYPE: (BUILD,FIX,CHANGE):BUILD
    
    Task: (why was this change made):scrollable nav bar needed
    
    Changes: (what was done to address problem):
    
    Note:

--- 1. Main/main.js
+++ 1. Main/main.js
@@ -1,12 +1,18 @@
 // change background to red wit js
 
-function run(){
-let newbg = document.getElementsByClassName('demo')
 
-newbg.textContent='hi, Im new '
-console.log(newbg)
+// opening hamburger menubar 
+// set add open class to button if clicked and on close
+// else listen for click and remove open class
+
+// get element by ID select hamburger menu and attribute(z-index *-1)
+
+const menuBtnOpen = false;
 
 
-
-
+function menuTgl(){
+    let menuTgl=false;
+    if(!menuTgl)=false{add to class open};
+    else{remove class open};
+    
 }
